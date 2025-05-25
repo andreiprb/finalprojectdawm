20c7d6770bf0f9b4cd5a8adc17351d601fd02a41
andreiprb <andreipriboi@icloud.com>
2025-05-25 16:31:56 +0300

    fixed db service? to investigate

--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -2,18 +2,29 @@ import { Injectable } from '@angular/core';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { environment } from '../../environments/environment';
 
+const lockNoOp = async <T>(
+  _lockName: string,
+  _acquireTimeout: number,
+  fn: () => Promise<T>
+): Promise<T> => {
+  return await fn()
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
-  private static supabaseClient: SupabaseClient;
+  private static supabaseClient: SupabaseClient = createClient(
+    environment.supabaseUrl,
+    environment.supabaseAnonKey,
+    {
+      auth: {
+        lock: lockNoOp
+      }
+    }
+  );
 
-  constructor() {
-    DatabaseService.supabaseClient = createClient(
-      environment.supabaseUrl,
-      environment.supabaseAnonKey
-    );
-  }
+  constructor() {}
 
   getClient(): SupabaseClient {
     return DatabaseService.supabaseClient;
