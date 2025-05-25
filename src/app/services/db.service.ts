f5ec8a9b5dd84c1a41da57f889a2bfa2045c8e8c
andreiprb <andreipriboi@icloud.com>
2025-05-25 16:53:55 +0300

    still tryna fix ts

--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -2,26 +2,13 @@ import { Injectable } from '@angular/core';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { environment } from '../../environments/environment';
 
-const lockNoOp = async <T>(
-  _lockName: string,
-  _acquireTimeout: number,
-  fn: () => Promise<T>
-): Promise<T> => {
-  return await fn()
-}
-
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
   private static supabaseClient: SupabaseClient = createClient(
     environment.supabaseUrl,
-    environment.supabaseAnonKey,
-    {
-      auth: {
-        lock: lockNoOp
-      }
-    }
+    environment.supabaseAnonKey
   );
 
   constructor() {}
