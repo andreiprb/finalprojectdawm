1b8d0b5cff005e0dbdfedfe89e3059d6456fcd71
andreiprb <andreipriboi@icloud.com>
2025-06-23 17:24:37 +0300

    clean code 2

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -3,29 +3,20 @@ import { CanActivate, Router } from '@angular/router';
 import { Observable, map, switchMap, timer } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthGuard implements CanActivate {
-
+abstract class BaseAuthGuard implements CanActivate {
   constructor(
-    private authService: AuthService,
-    private router: Router
+    protected authService: AuthService,
+    protected router: Router
   ) {}
 
   canActivate(): Observable<boolean> {
     return this.waitForAuthInit().pipe(
       switchMap(() => this.authService.currentUser$),
-      map(user => {
-        if (user) {
-          return true;
-        } else {
-          this.router.navigate(['/login']);
-          return false;
-        }
-      })
+      map(user => this.checkAccess(!!user))
     );
   }
+
+  protected abstract checkAccess(isAuthenticated: boolean): boolean;
 
   private waitForAuthInit(): Observable<boolean> {
     return timer(0, 100).pipe(
@@ -33,42 +24,31 @@ export class AuthGuard implements CanActivate {
         await this.authService.waitForInitialization();
         return true;
       }),
-      map(() => true)
-    );
+      map(() => true),
+    )
   }
 }
 
-@Injectable({
-  providedIn: 'root'
-})
-export class LoginGuard implements CanActivate {
-
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) {}
-
-  canActivate(): Observable<boolean> {
-    return this.waitForAuthInit().pipe(
-      switchMap(() => this.authService.currentUser$),
-      map(user => {
-        if (!user) {
-          return true;
-        } else {
-          this.router.navigate(['/dashboard']);
-          return false;
-        }
-      })
-    );
+@Injectable({ providedIn: 'root' })
+export class AuthGuard extends BaseAuthGuard {
+  protected checkAccess(isAuthenticated: boolean): boolean {
+    if (isAuthenticated) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
   }
+}
 
-  private waitForAuthInit(): Observable<boolean> {
-    return timer(0, 100).pipe(
-      switchMap(async () => {
-        await this.authService.waitForInitialization();
-        return true;
-      }),
-      map(() => true)
-    );
+@Injectable({ providedIn: 'root' })
+export class LoginGuard extends BaseAuthGuard {
+  protected checkAccess(isAuthenticated: boolean): boolean {
+    if (!isAuthenticated) {
+      return true;
+    } else {
+      this.router.navigate(['/dashboard']);
+      return false;
+    }
   }
 }
